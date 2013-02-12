1873629ff23f00fab64e0b6551aa37ea818691a4
Erawat Chamanont <erawat@gmail.com>
2013-02-12 17:03:44 +0000

    added contact id so permission would work

--- CRM/Contactcalendar/Page/Calendar.php
+++ CRM/Contactcalendar/Page/Calendar.php
@@ -3,10 +3,13 @@
 require_once 'CRM/Core/Page.php';
 
 class CRM_Contactcalendar_Page_Calendar extends CRM_Core_Page {
+
+  public $_contactId = null;
+
   function run() {
     
-    $contactId = CRM_Utils_Request::retrieve( 'cid', 'Positive', $this, true );
-    $this->assign( 'contactId', $contactId );
+    $this->_contactId = CRM_Utils_Request::retrieve( 'cid', 'Positive', $this, true );
+    $this->assign( 'contactId', $this->_contactId );
 
     // check logged in url permission
     require_once 'CRM/Contact/Page/View.php';
