5bdcbf47bb79d90dfba21e94b440afa5369be36e
mehalforc <sdberndt@charter.net>
2016-11-18 09:12:52 -0600

    Update Calendar.php

--- CRM/Contactcalendar/BAO/Calendar.php
+++ CRM/Contactcalendar/BAO/Calendar.php
@@ -5,9 +5,10 @@ class CRM_Contactcalendar_BAO_Calendar{
 	/**
 	*	Get Calendar event by $contactId
 	*/
-	static function getActivities($contactId, $start, $end, $atypes, $aemailed){
+	//static function getActivities($contactId){
+	static function getActivities($contactId, $start, $end, $show, $hide){
 
-	$queryParam = array(1 => array($contactId, 'Integer'), 2 => array($start, 'String'), 3 => array($end, 'String'), 4 => array($atypes, 'String'));
+	$queryParam = array(1 => array($contactId, 'Integer'), 2 => array($start, 'String'), 3 => array($end, 'String'));
 
 $query1 = "SELECT activity.id,
 activity.subject,
@@ -20,9 +21,9 @@ LEFT JOIN civicrm_activity_contact target ON target.activity_id = activity.id
 WHERE (assignment.contact_id = %1 OR target.contact_id = %1)
 AND activity.activity_date_time BETWEEN %2 AND %3";
 
-$query2 = "AND activity.activity_type_id IN (%4)";
+$query2 = "AND activity.activity_type_id IN ($show)";
 
-$query3 = "AND activity.status_id != 3";
+$query3 = "AND activity.activity_type_id NOT IN ($hide)";
 
 $query4 = "AND activity.is_deleted = 0
 AND activity.is_current_revision = 1
@@ -31,8 +32,8 @@ GROUP BY activity.id";
 
 $query = $query1;
 
-if(strtolower($atypes) != "all"){$query = $query." ".$query2;}
-if($aemailed == 0){$query = $query." ".$query3;}
+if(strtolower($show) != "all"){$query = $query." ".$query2;}
+if($hide != "0"){$query = $query." ".$query3;}
 
 $query = $query." ".$query4;
 
@@ -46,7 +47,6 @@ $query = $query." ".$query4;
 	$activities[$dao->id]['status_id'] =  $dao->status_id;
 	$activities[$dao->id]['activity_date_time'] =  $dao->activity_date_time;
     }
-
 		return $activities;
 	}
 }
